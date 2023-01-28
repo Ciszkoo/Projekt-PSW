0b69bda734227063318133304097e6121cba9249
Ciszkoo <pawel.olszko.p@gmail.com>
2023-01-28 01:00:45 +0100

    server: fix on creating user

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -2,19 +2,27 @@ import { Request, Response } from "express";
 import { UserModel } from "../models/user.model";
 import { RegisterUserInput } from "../schema/user.schema";
 import { hashPassword } from "../utils/hashPassword";
+import logger from "../utils/logger";
 
 export const handleRegister = async (
   req: Request<{}, {}, RegisterUserInput>,
   res: Response
 ) => {
   const { email } = req.body;
-  const userDB = await UserModel.findOne({ email });
-  if (userDB) {
-    res.status(400).send({ message: "User already exists" });
+  logger.info(email);
+  try {
+    const userDB = await UserModel.findOne({ email }).catch((err) => {
+      null;
+    });
+    if (userDB) {
+      return res.status(400).send({ message: "User already exists" });
+    }
+  } catch (error) {
+    logger.error(error);
   }
   const password = await hashPassword(req.body.password);
   const newUser = await UserModel.create({ ...req.body, password: password });
-  res.status(201).send({ message: "User created" });
+  return res.status(201).send({ message: "User created" });
 };
 
 export const handleGetUser = async (req: Request, res: Response) => {
